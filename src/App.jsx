8f7563523587273438492b90dadf78f2815087b4
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-13 23:50:00 +0300

    need to replan

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./Components/Homepage Components/Homepage";
 
 function App() {
-  return (
-    <Router>
-      <Homepage />
-    </Router>
-  );
+  return <Homepage />;
 }
 
 export default App;
