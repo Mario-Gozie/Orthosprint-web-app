246eec9600b8ff6a59b92773568846cb98fef8cc
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-14 01:31:27 +0300

    working on buttons

--- src/Components/Homepage-Components/LoginButton.jsx
+++ src/Components/Homepage-Components/LoginButton.jsx
@@ -1,21 +1,35 @@
 import React from "react";
-import { ArrowRightCircle, ChevronRight } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 
 function LoginButton() {
   return (
     <div
       style={{
-        width: "8rem",
+        width: "8.5rem",
         height: "3rem",
-        background: "red",
+        background: "black",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         borderRadius: "25px",
+        padding: "0.1rem",
       }}
     >
-      <ChevronRight background="white" strokeWidth={3} />
-      <p>Get in touch</p>
+      <div
+        style={{
+          background: "white",
+          borderRadius: "50%",
+          width: "2rem",
+          height: "2rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginRight: "0.2rem",
+        }}
+      >
+        <ChevronRight background="white" strokeWidth={3} />
+      </div>
+      <p style={{ color: "white" }}>Get in touch</p>
     </div>
   );
 }
