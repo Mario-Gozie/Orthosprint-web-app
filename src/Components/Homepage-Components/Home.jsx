5947fb96e8768113c2463aec2b0d4f97d5b4858c
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-19 04:38:07 +0300

    The hero page looks a bit fair now

--- src/Components/Homepage-Components/Home.jsx
+++ src/Components/Homepage-Components/Home.jsx
@@ -3,16 +3,19 @@ import React from "react";
 function Home() {
   return (
     <section style={{ height: "100vh" }} id="home">
-      <h2>Orthosprint Orthotics and Prosthetics</h2>
-      <h1>Empowering Your Journey to Progress</h1>
+      <div className="overlay"></div>
+      <div className="text">
+        <h2>Orthosprint Orthotics and Prosthetics</h2>
+        <h1>Empowering Your Journey to Progress</h1>
 
-      <p>
-        At our Prosthetic and Orthotic Centre in Abia State, Nigeria, we offer
-        personalized solutions to enhance mobility and restore independence. Our
-        experienced team is dedicated to empowering individuals with the support
-        they need to improve their quality of life. Start your journey to
-        greater freedom today!
-      </p>
+        <p>
+          At our Prosthetic and Orthotic Centre in Abia State, Nigeria, we offer
+          personalized solutions to enhance mobility and restore independence.
+          Our experienced team is dedicated to empowering individuals with the
+          support they need to improve their quality of life. Start your journey
+          to greater freedom today!
+        </p>
+      </div>
     </section>
   );
 }
