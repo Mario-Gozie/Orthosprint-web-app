196a2a8f1c3321a004a72b0643c9eec2edc80290
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-21 01:43:50 +0300

    worked on getting the json file for services

--- src/Components/Homepage-Components/FooterQuickLink.jsx
+++ src/Components/Homepage-Components/FooterQuickLink.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import HomePageNavItems from "./HomePageNavItems";
-import navItems from "../../Data/jsonData/navItems.json";
 
 function FooterQuickLink() {
   return (
     <div>
       <h2>Quick Links</h2>
-      <HomePageNavItems linkType="Quicklink" />
+      <HomePageNavItems type="Quicklink" />
       {/* <ul className="quick-links">
         {navItems.map((navItem) => {
           return <li></li>;
