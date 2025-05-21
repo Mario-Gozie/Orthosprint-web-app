3d2c5593dd26d9b95bdf60f5e26be63e4582351a
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-21 12:18:39 +0300

    now the footer logic is fair

--- src/Components/Homepage-Components/FooterServices.jsx
+++ src/Components/Homepage-Components/FooterServices.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import services from "../../Data/jsonData/services.json";
+import { Link } from "react-scroll";
 
 function FooterServices() {
-  return <div>FooterServices</div>;
+  return (
+    <div>
+      <h2>Services</h2>
+      <ul>
+        {services.map((service) => {
+          return (
+            <li>
+              <Link
+                to="services"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                //   activeClass={activeClass}
+                //   className={`${type}-nav-link`}
+                //   onClick={onclick}
+              >
+                {service.service}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default FooterServices;
