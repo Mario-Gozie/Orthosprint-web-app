ddb20c3c4571aa8b9276d7714b93b8716035425f
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-21 12:06:57 +0300

    worked on the contact area

--- src/Components/Homepage-Components/FooterContact.jsx
+++ src/Components/Homepage-Components/FooterContact.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import contact from "../../Data/jsonData/contact.json";
 
 function FooterContact() {
-  return <div>FooterContact</div>;
+  return (
+    <div>
+      <h2>Contact Us</h2>
+
+      <div>
+        <p>
+          {contact.address.street}, {contact.address.district}
+        </p>
+        <p>
+          {contact.address.city}, {contact.address.state}{" "}
+          {contact.address.zipCode}
+        </p>
+        <p>Phone: {contact.contact.phone}</p>
+        <p>Email: {contact.contact.email}</p>
+        <p>
+          {contact.hoursOfOperation.days}: {contact.hoursOfOperation.time}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default FooterContact;
