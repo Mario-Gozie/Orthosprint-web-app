cb52d5df33d4b8b61fe43fec38c0c0eeb17c49db
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-21 13:43:32 +0300

    fixed the icon importation issue

--- src/Components/Homepage-Components/ServicesCards.jsx
+++ src/Components/Homepage-Components/ServicesCards.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import services from "../../Data/jsonData/services.json";
-import { FaRobot, FaChild, FaHandsHelping, FaTools } from "react-icons/fa";
-import { Md3DRotation, MdMedicalServices } from "react-icons/md";
-import { GiOrthotic } from "react-icons/gi";
+import {
+  FaRobot,
+  FaChild,
+  FaHandsHelping,
+  FaTools,
+  FaCrutch,
+  FaCube,
+} from "react-icons/fa";
+import { Md3dRotation } from "react-icons/md";
 
 function ServicesCards() {
   // Map each service to an icon component
   const serviceIcons = {
-    "Custom Prosthtics": <FaRobot />,
-    "Orthotic Devices": <GiOrthotic />,
-    "Pediatric Care": <FaChild />,
-    "3D Scanning Technology": <Md3DRotation />,
-    "Rehabilitation Support": <FaHandsHelping />,
-    "Maintenance & Adjustments": <FaTools />,
+    "Custom Prosthtics": <FaRobot size={40} color="#3a86ff" />,
+    "Orthotic Devices": <FaCrutch size={40} color="#3a86ff" />,
+    "Pediatric Care": <FaChild size={40} color="#3a86ff" />,
+    "3D Scanning Technology": <Md3dRotation size={40} color="#3a86ff" />,
+
+    "Rehabilitation Support": <FaHandsHelping size={40} color="#3a86ff" />,
+    "Maintenance & Adjustments": <FaTools size={40} color="#3a86ff" />,
   };
 
   return (
