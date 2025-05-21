f1bece9350d617ca3cad24d997534fe0ec7a0f5e
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-21 12:53:21 +0300

    still working a bit on the footer

--- src/Components/Homepage-Components/Footer.jsx
+++ src/Components/Homepage-Components/Footer.jsx
@@ -3,14 +3,22 @@ import FooterAbout from "./FooterAbout";
 import FooterQuickLink from "./FooterQuickLink";
 import FooterContact from "./FooterContact";
 import FooterServices from "./FooterServices";
+import contact from "../../Data/jsonData/contact.json";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <section>
-      <FooterAbout />
-      <FooterQuickLink />
-      <FooterServices />
-      <FooterContact />
+      <div>
+        <FooterAbout />
+        <FooterQuickLink />
+        <FooterServices />
+        <FooterContact />
+      </div>
+      <hr />
+      <p>
+        &copy; {currentYear} {contact.companyName}. All Rights Reserved
+      </p>
     </section>
   );
 }
