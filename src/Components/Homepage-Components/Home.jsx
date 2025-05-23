7aaa3809703b0fb638555200464b9c86d660360e
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-24 01:01:47 +0300

    making a better home section

--- src/Components/Homepage-Components/Home.jsx
+++ src/Components/Homepage-Components/Home.jsx
@@ -3,18 +3,27 @@ import React from "react";
 function Home() {
   return (
     <section style={{ height: "100vh" }} id="home">
-      <div className="overlay"></div>
-      <div className="text">
-        <h2>Orthosprint Orthotics and Prosthetics</h2>
-        <h1>Empowering Your Journey to Progress</h1>
+      <div className="home-wrapper">
+        <div className="clockwise-rotation"></div>
+        <div className="anti-clockwise-rotation"></div>
+        <div className="centre"></div>
+        <div className="text">
+          <h2>Orthosprint Orthotics and Prosthetics</h2>
+          <h1>
+            Empowering Your <br />
+            Journey to Progress
+          </h1>
 
-        <p>
-          At our Prosthetic and Orthotic Centre in Abia State, Nigeria, we offer
-          personalized solutions to enhance mobility and restore independence.
-          Our experienced team is dedicated to empowering individuals with the
-          support they need to improve their quality of life. Start your journey
-          to greater freedom today!
-        </p>
+          <p>
+            At our Prosthetic and Orthotic Centre in Abia State, Nigeria, we
+            offer personalized solutions to enhance mobility and restore
+            independence. Our experienced team is dedicated to empowering
+            individuals with the support they need to improve their quality of
+            life. Start your journey to greater freedom today!
+          </p>
+          <div></div>
+        </div>
+        <img src="" alt="" />
       </div>
     </section>
   );
