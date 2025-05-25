f0e3f0f3d39a1697cf53b6b5806271d26a71e3c6
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-26 01:32:57 +0300

    home page looks better now

--- src/Components/Homepage-Components/Home.jsx
+++ src/Components/Homepage-Components/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { BsArrowUpRightCircle } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -16,6 +18,17 @@ function Home() {
             individuals with the support they need to improve their quality of
             life. Start your journey to greater freedom today!
           </p>
+          <div className="consultation-button-container">
+            <div className="dot"></div>
+            <div className="straight-line"></div>
+            <Link to="/admin" className="consultation-button">
+              <div className="button-text">Book consultation</div>
+              <BsArrowUpRightCircle
+                className="circle-arrow"
+                style={{ fontSize: "32px" }}
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </section>
