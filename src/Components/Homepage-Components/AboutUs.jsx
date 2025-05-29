d033e26f7b57c5ff0e5156ae8073b33fa8384818
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-29 08:15:20 +0300

    working on the about page

--- src/Components/Homepage-Components/AboutUs.jsx
+++ src/Components/Homepage-Components/AboutUs.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function AboutUs() {
   return (
     <section style={{ height: "100vh" }} id="about">
-      <div className="about-container">About Us</div>
+      <div className="about-container">
+        <div className="about-img-container"></div>
+        <div className="About-text-container"></div>
+      </div>
     </section>
   );
 }
