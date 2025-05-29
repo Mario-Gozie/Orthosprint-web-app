a5ca9be9a03aff3dc383f1016269f5415214ab98
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-29 08:21:42 +0300

    working on the about gradually

--- src/Components/Homepage-Components/AboutUs.jsx
+++ src/Components/Homepage-Components/AboutUs.jsx
@@ -5,7 +5,11 @@ function AboutUs() {
     <section style={{ height: "100vh" }} id="about">
       <div className="about-container">
         <div className="about-img-container"></div>
-        <div className="About-text-container"></div>
+        <div className="About-text-container">
+          <h2>About us</h2>
+          <h3>The Great Place for Prosthetic and Orthotic Services</h3>
+          <div className="about-special-list"></div>
+        </div>
       </div>
     </section>
   );
