547f3c5e140c4c44872c9d8b1ad48e7636d2ffc5
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-30 18:38:51 +0300

    working on the about page to make it better

--- src/Components/Homepage-Components/AboutUs.jsx
+++ src/Components/Homepage-Components/AboutUs.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import aboutImg1 from "../../assets/aboutImg1.jpg";
+import aboutImg2 from "../../assets/aboutImg2.jpg";
 import AboutServiceList from "./AboutServiceList";
 
 function AboutUs() {
   return (
     <section id="about">
       <div className="about-container" style={{ height: "100%" }}>
-        <div className="about-img-container"></div>
+        <div className="about-img-container">
+          <img src={aboutImg1} alt="" />
+          <img src={aboutImg2} alt="" />
+        </div>
         <div className="about-text-container">
           <h2>About us</h2>
           <h3>The Great Place for Prosthetic and Orthotic Services</h3>
