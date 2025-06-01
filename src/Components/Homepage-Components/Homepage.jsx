c63434a0fd24a75e4aad5b495d1671876a4d4f12
Mario-Gozie <oguedoihucg@gmail.com>
2025-06-02 01:18:15 +0300

    I was able to access the staff data

--- src/Components/Homepage-Components/Homepage.jsx
+++ src/Components/Homepage-Components/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Home from "./Home";
 import AboutUs from "./AboutUs";
@@ -12,12 +12,35 @@ import "../../Css/homepageCss.css";
 import supabase from "../../config/supabaseClient.js";
 
 function Homepage() {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null);
+
+  const [staff, setStaff] = useState(null);
+
+  useEffect(() => {
+    const fetchStaff = async () => {
+      const { data, error } = await supabase.from("Orthosprint_Team").select();
+
+      if (error) {
+        setFetchError(`Could not fetch the staff`);
+        setStaff(null);
+        console.log(error);
+        console.log(fetchError);
+      }
+
+      if (data) {
+        setStaff(data);
+        setFetchError(null);
+        console.log(data);
+      }
+    };
+    fetchStaff();
+  }, []);
   return (
     <>
       <Nav />
       <Home />
       <AboutUs />
+      {staff && <p>{staff[0].id}</p>}
       <Services />
       <Testimonies />
       <Contact />
