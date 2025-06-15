30e7b925257c677607dc4bc6eab5fe623c4d4346
Mario-Gozie <oguedoihucg@gmail.com>
2025-06-15 22:38:39 +0300

    I am working on the date format

--- src/Components/Customer-App-Components/TimePicker.jsx
+++ src/Components/Customer-App-Components/TimePicker.jsx
@@ -5,16 +5,20 @@ const TimeSlotPicker = ({ availableSlots, onTimeSelect }) => {
 
   const handleSlotClick = (slot) => {
     setSelectedSlot(slot);
-    onTimeSelect(slot); // Pass selected time to parent
+    // onTimeSelect(slot); // Pass selected time to parent
   };
 
   // Sample time slots (replace with your actual data)
   const timeSlots = availableSlots || [
+    "08:00 AM",
     "09:00 AM",
-    "10:30 AM",
+    "10:00 AM",
+    "11:00 AM",
     "12:00 PM",
+    "01:00 PM",
     "02:00 PM",
-    "03:30 PM",
+    "03:00 PM",
+    "04:00 PM",
     "05:00 PM",
   ];
 
