d1c4ff42b4a03904c281dd1edb23a1595c836423
Mario-Gozie <oguedoihucg@gmail.com>
2025-06-18 10:05:50 +0300

    setting up path for the login and sign up

--- src/Components/Customer-App-Components/CustomerApp.jsx
+++ src/Components/Customer-App-Components/CustomerApp.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
+const Login = lazy(() => import("./Login"));
+const SignUp = lazy(() => import("./SignUp"));
 const Dashboard = lazy(() => import("./Dashboard"));
 const Profile = lazy(() => import("./Profile"));
 const Notification = lazy(() => import("./Notification"));
@@ -11,10 +13,15 @@ function CustomerApp() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
         <Route path="/:id/dashboard" element={<Dashboard />} />
-        <Route path="/:id/Profile" element={<Profile />} />
+        <Route path="/:id/profile" element={<Profile />} />
         <Route path="/:id/notification" element={<Notification />} />
         <Route path="/:id/booking" element={<Booking />} />
+
+        {/* This below is my default path */}
+        <Route index element={<Dashboard />} />
       </Routes>
     </Suspense>
   );
