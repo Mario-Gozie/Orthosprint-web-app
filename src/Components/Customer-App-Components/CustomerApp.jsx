425e133cb5595d9f5d0157a314d9d202fc008f0b
Mario-Gozie <oguedoihucg@gmail.com>
2025-06-27 23:46:04 +0300

    will work on improvement

--- src/Components/Customer-App-Components/CustomerApp.jsx
+++ src/Components/Customer-App-Components/CustomerApp.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { lazy, Suspense } from "react";
 
 import { Routes, Route, Navigate } from "react-router-dom";
-import { AuthRoutes } from "./AuthRoutes";
-
-import { AuthProvider } from "./AuthProvider";
 
 const CustomerAuthPage = lazy(() => import("./CustomerAuthPage"));
 const Dashboard = lazy(() => import("./Dashboard"));
@@ -15,79 +12,77 @@ const EditAppointment = lazy(() => import("./EditAppointment"));
 
 function CustomerApp() {
   return (
-    <AuthProvider>
-      <Suspense fallback={<div className="loading-spinner">Loading...</div>}>
-        <Routes>
-          {/* Public routes */}
-          <Route
-            path="/login"
-            element={
-              <AuthRoutes.Public>
-                <CustomerAuthPage />
-              </AuthRoutes.Public>
-            }
-          />
+    <Suspense fallback={<div className="loading-spinner">Loading...</div>}>
+      <Routes>
+        {/* Public routes */}
+        <Route
+          path="/login"
+          element={
+            <AuthRoutes.Public>
+              <CustomerAuthPage />
+            </AuthRoutes.Public>
+          }
+        />
 
-          {/* Protected routes */}
-          <Route
-            path="/:id/dashboard"
-            element={
-              <AuthRoutes.Private>
-                <Dashboard />
-              </AuthRoutes.Private>
-            }
-          />
-          <Route
-            path="/:id/profile"
-            element={
-              <AuthRoutes.Private>
-                <Profile />
-              </AuthRoutes.Private>
-            }
-          />
-          <Route
-            path="/:id/notification"
-            element={
-              <AuthRoutes.Private>
-                <Notification />
-              </AuthRoutes.Private>
-            }
-          />
-          <Route
-            path="/:id/booking"
-            element={
-              <AuthRoutes.Private>
-                <Booking />
-              </AuthRoutes.Private>
-            }
-          />
-          <Route
-            path="/booking/:appointmentId/edit"
-            element={
-              <AuthRoutes.Private>
-                <EditAppointment />
-              </AuthRoutes.Private>
-            }
-          />
+        {/* Protected routes */}
+        <Route
+          path="/:id/dashboard"
+          element={
+            <AuthRoutes.Private>
+              <Dashboard />
+            </AuthRoutes.Private>
+          }
+        />
+        <Route
+          path="/:id/profile"
+          element={
+            <AuthRoutes.Private>
+              <Profile />
+            </AuthRoutes.Private>
+          }
+        />
+        <Route
+          path="/:id/notification"
+          element={
+            <AuthRoutes.Private>
+              <Notification />
+            </AuthRoutes.Private>
+          }
+        />
+        <Route
+          path="/:id/booking"
+          element={
+            <AuthRoutes.Private>
+              <Booking />
+            </AuthRoutes.Private>
+          }
+        />
+        <Route
+          path="/booking/:appointmentId/edit"
+          element={
+            <AuthRoutes.Private>
+              <EditAppointment />
+            </AuthRoutes.Private>
+          }
+        />
 
-          {/* Default routes */}
-          <Route
-            index
-            element={
-              <AuthRoutes.Private>
-                <Dashboard />
-              </AuthRoutes.Private>
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <div className="not-found">Customer App 404 - Page not found</div>
-            }
-          />
-        </Routes>
-      </Suspense>
-    </AuthProvider>
+        {/* Default routes */}
+        <Route
+          index
+          element={
+            <AuthRoutes.Private>
+              <Dashboard />
+            </AuthRoutes.Private>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <div className="not-found">Customer App 404 - Page not found</div>
+          }
+        />
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/Components/Customer-App-Components/CustomerAuthPage.jsx
+++ src/Components/Customer-App-Components/CustomerAuthPage.jsx
@@ -1,107 +1,107 @@
-// import React from "react";
-// import Login from "./Login";
-// import AuthPageNav from "./AuthPageNav";
-// import SignUp from "./SignUp";
-// import clsx from "clsx";
-// import { FaBone } from "react-icons/fa";
-// import { AnimatePresence } from "framer-motion";
-// import { useState } from "react";
-// import FormDisplayAnimation from "./formDisplayAnimation";
-// import {
-//   useLocation,
-//   useNavigate,
-//   Routes,
-//   Route,
-//   Link,
-// } from "react-router-dom";
+import React from "react";
+import Login from "./Login";
+import AuthPageNav from "./AuthPageNav";
+import SignUp from "./SignUp";
+import clsx from "clsx";
+import { FaBone } from "react-icons/fa";
+import { AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import FormDisplayAnimation from "./formDisplayAnimation";
+import {
+  useLocation,
+  useNavigate,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
-// import "../../Css/CustomerAppCss.css";
+import "../../Css/CustomerAppCss.css";
 
-// function CustomerAuthPage() {
-//   const location = useLocation(); // Get current route location
-//   const navigate = useNavigate();
-//   const [activeForm, setActiveForm] = useState("login");
-//   const [showClientPassword, setShowClientPassword] = useState(false);
-//   const [clientPassword, setClientPassword] = useState(""); // State for password
-//   const [showNewPassword, setShowNewPassword] = useState(false);
-//   const [newPassword, setNewPassword] = useState(""); // State for password
-//   const [confirmPassword, setConfirmPassword] = useState(""); // State for password
-//   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-//   return (
-//     <div className="login-signUp">
-//       <AuthPageNav />
-//       <div className="main-login-signUp-container">
-//         <div className="text-login-signUp">
-//           <div className="text-bone-container">
-//             <div className="bone-icon-container">
-//               <FaBone className="bone-Icon" />
-//             </div>
-//             <div className="login-signup-text">
-//               <h3>Orthosprint P&O Centre</h3>
-//               <p>Prosthetic & Orthotic Excellence</p>
-//             </div>
-//           </div>
+function CustomerAuthPage() {
+  const location = useLocation(); // Get current route location
+  const navigate = useNavigate();
+  const [activeForm, setActiveForm] = useState("login");
+  const [showClientPassword, setShowClientPassword] = useState(false);
+  const [clientPassword, setClientPassword] = useState(""); // State for password
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [newPassword, setNewPassword] = useState(""); // State for password
+  const [confirmPassword, setConfirmPassword] = useState(""); // State for password
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  return (
+    <div className="login-signUp">
+      <AuthPageNav />
+      <div className="main-login-signUp-container">
+        <div className="text-login-signUp">
+          <div className="text-bone-container">
+            <div className="bone-icon-container">
+              <FaBone className="bone-Icon" />
+            </div>
+            <div className="login-signup-text">
+              <h3>Orthosprint P&O Centre</h3>
+              <p>Prosthetic & Orthotic Excellence</p>
+            </div>
+          </div>
 
-//           <div className="login-signUp-button-container">
-//             <div className="login-signUp-button-wrapper">
-//               <div className="login-signUp-buttons">
-//                 <Link
-//                   to="app/login"
-//                   className={clsx("login", {
-//                     "active-btn": location.pathname === "/login",
-//                   })}
-//                 >
-//                   Login
-//                 </Link>
-//                 <Link
-//                   to="/signUp"
-//                   className={clsx("signUp", {
-//                     "active-btn": location.pathname === "/signUp",
-//                   })}
-//                 ></Link>
-//               </div>
+          <div className="login-signUp-button-container">
+            <div className="login-signUp-button-wrapper">
+              <div className="login-signUp-buttons">
+                <Link
+                  to="app/login"
+                  className={clsx("login", {
+                    "active-btn": location.pathname === "/login",
+                  })}
+                >
+                  Login
+                </Link>
+                <Link
+                  to="/signUp"
+                  className={clsx("signUp", {
+                    "active-btn": location.pathname === "/signUp",
+                  })}
+                ></Link>
+              </div>
 
-//               <AnimatePresence mode="wait">
-//                 <Routes location={location} key={location.pathname}>
-//                   <Route
-//                     path="/login"
-//                     element={
-//                       <FormDisplayAnimation>
-//                         <Login
-//                           showClientPassword={showClientPassword}
-//                           setShowClientPassword={setShowClientPassword}
-//                           clientPassword={clientPassword}
-//                           setClientPassword={setClientPassword}
-//                         />
-//                       </FormDisplayAnimation>
-//                     }
-//                   />
-//                   <Route
-//                     path="/signUp"
-//                     element={
-//                       <FormDisplayAnimation>
-//                         <SignUp
-//                           showNewPassword={showNewPassword}
-//                           setShowNewPassword={setShowNewPassword}
-//                           newPassword={newPassword}
-//                           setNewPassword={setNewPassword}
-//                           confirmPassword={confirmPassword}
-//                           setConfirmPassword={setConfirmPassword}
-//                           setShowConfirmPassword={setShowConfirmPassword}
-//                           showConfirmPassword={showConfirmPassword}
-//                         />
-//                       </FormDisplayAnimation>
-//                     }
-//                   />
-//                   {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
-//                 </Routes>
-//               </AnimatePresence>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+              <AnimatePresence mode="wait">
+                <Routes location={location} key={location.pathname}>
+                  <Route
+                    path="/login"
+                    element={
+                      <FormDisplayAnimation>
+                        <Login
+                          showClientPassword={showClientPassword}
+                          setShowClientPassword={setShowClientPassword}
+                          clientPassword={clientPassword}
+                          setClientPassword={setClientPassword}
+                        />
+                      </FormDisplayAnimation>
+                    }
+                  />
+                  <Route
+                    path="/signUp"
+                    element={
+                      <FormDisplayAnimation>
+                        <SignUp
+                          showNewPassword={showNewPassword}
+                          setShowNewPassword={setShowNewPassword}
+                          newPassword={newPassword}
+                          setNewPassword={setNewPassword}
+                          confirmPassword={confirmPassword}
+                          setConfirmPassword={setConfirmPassword}
+                          setShowConfirmPassword={setShowConfirmPassword}
+                          showConfirmPassword={showConfirmPassword}
+                        />
+                      </FormDisplayAnimation>
+                    }
+                  />
+                  {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
+                </Routes>
+              </AnimatePresence>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// export default CustomerAuthPage;
+export default CustomerAuthPage;
