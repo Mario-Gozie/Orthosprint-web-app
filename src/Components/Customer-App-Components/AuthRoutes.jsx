8b5052c47c1541a6a96d32a6d375890a3fff42d7
Mario-Gozie <oguedoihucg@gmail.com>
2025-07-07 03:07:05 +0300

    working on making the UI better

--- src/Components/Customer-App-Components/AuthRoutes.jsx
+++ src/Components/Customer-App-Components/AuthRoutes.jsx
@@ -1,21 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./AuthProvider"; // Import the useAuth hook
 
-// const PrivateRoute = ({ children }) => {
-//   const { isAuthenticated } = useAuth(); // Properly use the hook
-//   return isAuthenticated ? children : <Navigate to="/login" replace />;
-// };
-
-// const PublicRoute = ({ children }) => {
-//   const { isAuthenticated } = useAuth(); // Properly use the hook
-//   return !isAuthenticated ? children : <Navigate to="/:id/dashboard" replace />;
-// };
-
-// export const AuthRoutes = {
-//   Private: PrivateRoute,
-//   Public: PublicRoute,
-// };
-
 export const AuthRoutes = {
   Public: ({ children }) => {
     const { isAuthenticated } = useAuth();
--- src/Components/Customer-App-Components/FormDisplayAnimation.jsx
+++ src/Components/Customer-App-Components/FormDisplayAnimation.jsx
@@ -5,7 +5,7 @@ function FormDisplayAnimation({ children }) {
     <>
       <motion.div
         // key={activeForm}
-        initial={{ opacity: 0, y: 20 }}
+        initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -20 }}
         transition={{ duration: 0.3, ease: "easeInOut" }}
